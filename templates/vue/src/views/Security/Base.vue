dd1179203f28fc01c016e615b93842ab80a4cd6c
thanhdung96 <14dh110224@st.huflit.edu.vn>
2022-06-13 11:40:21 +0700

    Commented out unneccessary components. Closes #23
    Modifications qui seront validées :
    	modifié :         templates/vue/src/views/Security/Base.vue
    	modifié :         templates/vue/src/views/dashboard/Index.vue
    	modifié :         templates/vue/src/views/dashboard/components/core/AppBar.vue

--- templates/vue/src/views/Security/Base.vue
+++ templates/vue/src/views/Security/Base.vue
@@ -1,15 +1,15 @@
 <template>
-   <v-app >
-      <v-main>
-         <v-container fluid fill-height>
-            <v-layout align-center justify-center>
-               <v-flex xs12 sm8 md4>
-				   <router-view />
-               </v-flex>
-            </v-layout>
-         </v-container>
-      </v-main>
-   </v-app>
+	<v-app >
+		<v-main>
+			<v-container fluid fill-height>
+				<v-layout align-center justify-center>
+					<v-flex xs12 sm8 md4>
+						<router-view />
+					</v-flex>
+				</v-layout>
+			</v-container>
+		</v-main>
+	</v-app>
 </template>
 <script>
 export default {
