5993002ca6e574f4c9756cf6783b9a239e683524
thanhdung96 <14dh110224@st.huflit.edu.vn>
2022-07-04 17:05:35 +0700

    Adding icon
    Modifications qui seront validées :
    	modifié :         public/favicon.ico
    	modifié :         public/favicon.png
    	modifié :         src/assets/logo.png
    	modifié :         src/background.js
    	modifié :         src/views/dashboard/components/core/Drawer.vue

--- templates/vue/src/background.js
+++ templates/vue/src/background.js
@@ -15,6 +15,7 @@ async function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    icon:'public/favicon.ico',
     webPreferences: {
       
       // Use pluginOptions.nodeIntegration, leave this alone
